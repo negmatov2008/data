aa7a0f3220fb3ba5f8cf6dbb690b4644c7380d24
nodirdev984 <negmatovn984@gmail.com>
2024-02-01 20:34:24 +0500

    backend Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import { func } from "prop-types";
 import { useEffect, useState } from "react";
+import Page from "./Page";
 
 function App() {
-  const [user, setUser] = useState();
-  async function getApi() {
-    const res = await fetch("http://localhost:3000/user");
-    const data = await res.json();
-    console.log(data);
-    setUser(data[0]);
-  }
-  useEffect(() => {
-    getApi();
-    console.log(user);
-  }, []);
-
   return (
     <>
-      <h1>{user && user.name}</h1>
+      <Page />
     </>
   );
 }
